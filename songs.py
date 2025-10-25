f44a4623d7f057a5ad50cbecee5efd8133c3cd34
giothermal1234@gmail.com <giothermal1234@gmail.com>
2025-10-25 17:08:38 -0500

    longer notes 4 COM

--- songs.py
+++ songs.py
@@ -45,4 +45,7 @@ class SongManager():
 
     #Cats on Mars Attempt
     def cats_on_mars(self): #                    E       G       D110    B107    G       E
-        serial_util.send_as_bytes(b'\x8C\x00\x0F\x64\x10\x67\x10\x6e\x10\x6b\x10\x67\x10\x64\x10')
+        serial_util.send_as_bytes(b'\x8C\x00\x0F\x64\x10\x67\x10\x6e\x12\x6b\x10\x67\x10\x64\x12')
+
+        serial_util.send_as_bytes(b'\x8d\x01')
+
