0652eb595f23ef697e2f98d9e260e9e8d6e698f4
ateschan <ateschan@github.com>
2025-10-25 15:23:22 -0500

    added la cucaracha song

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+import time
 
 from pyPS4Controller.controller import Controller
 import serial
 
 ser = serial.Serial(port='/dev/irobot', baudrate=57600)
-
 
 class MyController(Controller):
     alog_up = 0
@@ -14,12 +14,20 @@ class MyController(Controller):
 
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
+    
+    def on_L1_press(self):
+        print("Selecting one song left")
+        
+    def on_R1_press(self):
+        print("Selecting one song right")
 
-    def on_R3_right(self, value):
-        send_as_bytes(b'\x8a\x01', ser)
+    # Brushes
+    #   def on_R3_right(self, value):
+    #        send_as_bytes(b'\x8a\x01', ser)
 
-    def on_R3_down(self, value):
-        send_as_bytes(b'\x8a\x02', ser)
+    # Motor
+    #   def on_R3_down(self, value):
+    #        send_as_bytes(b'\x8a\x02', ser)
 
     def on_circle_press(self):  # up
         send_as_bytes(b'\x89\xFE\x0C\x00\x00', ser)
@@ -32,6 +40,13 @@ class MyController(Controller):
 
     def on_x_press(self):  # right
         send_as_bytes(b'\x89\x27\x10\x00\x01', ser)
+
+
+    def on_share_press(self):
+        play_la_cucaracha(ser)
+
+    def on_playstation_button_press(self):
+        play_la_cucaracha(ser)
 
     def on_circle_release(self):
         send_as_bytes(b'\x89\x00\x00\x00\x00', ser)
@@ -109,12 +124,23 @@ class MyController(Controller):
 
 # Start - Programmatically play roomba notes
 
-
 def send_as_bytes(bytes, port):
     port.write(bytes)
 
+def play_la_cucaracha(port):
+    send_as_bytes(b'\x8C\x00\x0F\x37\x10\x37\x10\x37\x10\x3c\x1A\x40\x1A\x37\x10\x37\x10\x37\x10\x3c\x1A\x40\x1A\x3c\x10\x3c\x10\x3b\x10\x3b\x10\x39\x10\x39\x10\x37\x1A', ser)
+    send_as_bytes(b'\x8C\x01\x02\x39\x10\x37\x1A', ser)
+
+    send_as_bytes(b'\x8d\x00', ser)
+    # send_as_bytes(b'\x8d\x00', ser)
+
+    time.sleep(4.48)
+    print("FuckOff")
+
+    send_as_bytes(b'\x8d\x01', ser)
 
 def main():
+
     # passive
     send_as_bytes(b'\x80', ser)
     # safe mode
@@ -122,20 +148,11 @@ def main():
     # full control
     send_as_bytes(b'\x84', ser)
 
-    # send_as_bytes(b'\x8c\x00\x0A\x4c\10\x4c\10\x4c\10\x4c\10\x4c\10\x4c\10\x4c\10\x4c\10\x4c\10\x4c\10', ser)
-    # send_as_bytes(b'\x8C\x00\x0C\x3C\x10\x3C\x10\x3C\x10\x3E\x08\x40\x08\x41\x10\x40\x08\x3E\x08\x40\x10\x3C\x10\x40\x10\x43\x20', ser)
-    send_as_bytes(b'\x8C\x00\x03\x4C\x0A\x4C\x0A\x50\x20', ser)
-
-    # send_as_bytes(b'\x8d\x00', ser)
-    send_as_bytes(b'\x8d\x00', ser)
     # send_as_bytes(b'\x8c\x00', ser)
 
     controller = MyController(interface="/dev/input/js0",
                               connecting_using_ds4drv=False)
     controller.listen(400)
-
-    print("Hello world")
-
 
 if __name__ == "__main__":
     main()
