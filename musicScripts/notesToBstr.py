4b3d8430dae9d66e9097ac785b74274aaa5e0218
ateschan <ateschan@github.com>
2025-10-25 22:50:16 -0500

    upgraded script

--- musicScripts/notesToBstr.py
+++ musicScripts/notesToBstr.py
@@ -3,46 +3,54 @@ import sys
 
 octave = int(sys.argv[1])
 
-if ( octave < 0 ) or ( octave > 8) :
+if (octave < 0) or (octave > 8):
     print("Octave is too high or too low")
     exit(0)
 
 bstr = ''
+noteDuration = sys.argv[2]
 
-noteDuration = sys.argv[2] 
+for char in sys.argv[3].split(','):
+    print(char)
+    result = 0
+    if (char[0].isdigit()):
+        octave = int(char[0])
+        char = char[1:]
+        print(char)
+    else:
+        octave = int(sys.argv[1])
 
-for char in sys.argv[3]:
-    result = 0;
     match char:
         case 'G':
             result = 1 + octave * 12
-        case 'H': # G#
+        case 'G#':
             result = 2 + octave * 12
-        case 'A': # A
+        case 'A':
             result = 3 + octave * 12
-        case 'Q': # A#
+        case 'A#':
             result = 4 + octave * 12
-        case 'B': # B
+        case 'B':
             result = 5 + octave * 12
-        case 'C': # C
+        case 'C':
             result = 6 + octave * 12
-        case 'V': #C#
+        case 'C#':
             result = 7 + octave * 12
-        case 'D': #D
+        case 'D':
             result = 8 + octave * 12
-        case 'M': #D#
+        case 'D#':
             result = 9 + octave * 12
-        case 'E': #E
+        case 'E':
             result = 10 + octave * 12
-        case 'F': #F
+        case 'F':
             result = 11 + octave * 12
-        case 'Y': #F#
+        case 'F#':
             result = 12 + octave * 12
-        case 'R': #F#
+        case 'R':
             result = 100
+
     result = hex(int(result) + 30)
     bstr += '\\x' + result[2:]
-    if int(noteDuration) < 16 :
+    if int(noteDuration) < 16:
         bstr += '\\x0' + hex(int(noteDuration))[2:]
     else:
         bstr += '\\x' + hex(int(noteDuration))[2:]
