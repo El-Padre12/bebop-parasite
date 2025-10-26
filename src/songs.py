b6ca4b1a33637e755dc4f0d7a9ae4c9a242da6db
ateschan <ateschan@github.com>
2025-10-26 08:22:55 -0500

    fix

--- src/songs.py
+++ src/songs.py
@@ -4,7 +4,7 @@ import time
 
 class SongManager():
 
-    songAmount = 4
+    songAmount = 5
     currentSong = 0
 
     # TODO
@@ -38,7 +38,7 @@ class SongManager():
         serial_util.send_as_bytes(b'\x8C\x00\x0F\x37\x10\x37\x10\x37\x10\x3c\x1A\x40\x1A\x37\x10\x37\x10\x37\x10\x3c\x1A\x40\x1A\x3c\x10\x3c\x10\x3b\x10\x3b\x10\x39\x10\x39\x10\x37\x1A')
 
         # Second song section
-        serial_util.send_as_bytes(b'\x8C\x01\x01\x39\x1A')
+        serial_util.send_as_bytes(b'\x8C\x01\x02\x39\x10\x37\x1A')
 
         # Call first song
         serial_util.send_as_bytes(b'\x8d\x00')
@@ -74,7 +74,6 @@ class SongManager():
         serial_util.send_as_bytes(b'\x8c\x00\x02\x77\x18\x7c\x18')
 
         # call 1st song
-        serial_util.send_as_bytes(b'\x8d\x00')
         serial_util.send_as_bytes(b'\x8d\x00')
 
         print("Playing Mario Coin Sound!")
